ba1647c989d21e1f5c46b8012275dad61ba2f9dc
Phone <phone.madridista@gmail.com>
2018-10-27 12:07:32 +0700

    font-awesome & avoid caching

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function university_files(){
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
-    wp_enqueue_style('university_main_style', get_stylesheet_uri()); //Enqueue style.css
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
+    wp_enqueue_style('university_main_style', get_stylesheet_uri(),NULL,microtime()); //Enqueue style.css
+    wp_enqueue_style('font-awesome', get_theme_file_uri('/font-awesome/css/font-awesome.min.css'));
 }
 add_action('wp_enqueue_scripts', 'university_files');
