0105b42ed1c2b22321cdad02e9c00bfbc0aa8add
Phone <phone.madridista@gmail.com>
2018-10-28 07:44:05 +0700

    Menu of side child page menu link

--- page.php
+++ page.php
@@ -13,7 +13,7 @@
   </div>
   <div class="container container--narrow page-section">
   <?php
-    $theParent = wp_get_post_parent_id(get_the_ID());
+    $theParent = wp_get_post_parent_id(get_the_ID()); //To find parent ID
 
   if($theParent){ ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
@@ -21,14 +21,30 @@
     </div>
  <?php } ?>
 
-
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php
+    $childPage = get_pages(array(
+      'child_of'    =>  get_the_ID()
+    ));
+    if($theParent or $childPage){ ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+      <?php 
+      if($theParent){
+        $findChildrenOf = $theParent;
+      }else{
+        $findChildrenOf = get_the_ID();
+      }
+      wp_list_pages(array(
+        'title_li'    => NULL,
+        'child_of'    => $findChildrenOf
+      ));
+      ?>
+        <!-- <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li> -->
       </ul>
-    </div> -->
+    </div> 
+    <?php } ?>
     <div class="generic-content">
     <?php the_content(); ?>
     </div>
