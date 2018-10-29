ad1fe5ad81f6d07cd36090e7fe52b31136246903
Phone <phone.madridista@gmail.com>
2018-10-29 12:02:59 +0700

    custom field excerpt

--- front-page.php
+++ front-page.php
@@ -22,19 +22,21 @@
         while($homepageEvent->have_posts()){
           $homepageEvent->the_post() ?>
         <div class="event-summary">
-          <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+          <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
             <span class="event-summary__month"><?php the_time('M'); ?></span>
             <span class="event-summary__day"><?php the_time('d'); ?></span>  
           </a>
           <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php if(has_excerpt()){
+             echo get_the_excerpt();
+            }else{
+              echo wp_trim_words(get_the_content(), 18);
+            } ?><a href="<?php the_permalink(); ?>" class="nu gray"> Learn more</a></p>
           </div>
         </div>
-       <?php } ?>      
-   
-        
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+       <?php } ?>        
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
 
       </div>
     </div>
@@ -49,14 +51,18 @@
 
         while($homePagePosts->have_posts()){
           $homePagePosts->the_post() ?>
-             <div class="event-summary">
+        <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
             <span class="event-summary__month"><?php the_time('M'); ?></span>
             <span class="event-summary__day"><?php the_time('d'); ?></span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+            <p><?php if(has_excerpt()){
+             echo get_the_excerpt();
+            }else{
+              echo wp_trim_words(get_the_content(), 18);
+            } ?><a href="<?php the_permalink(); ?>" class="nu gray"> Learn more</a></p>
           </div>
         </div>
        <?php } wp_reset_postdata(); ?>        
