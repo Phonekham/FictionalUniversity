88154dcadf6336dd77c023d485c87b9e2b108300
Phone <phone.madridista@gmail.com>
2018-11-08 14:25:43 +0700

    Skip campus section coz can't create google api

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_files(){
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js', NULL, microtime(), true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     wp_enqueue_style('university_main_style', get_stylesheet_uri(),NULL,microtime()); //Enqueue style.css
     wp_enqueue_style('font-awesome', get_theme_file_uri('/font-awesome/css/font-awesome.min.css'));
--- header.php
+++ header.php
@@ -18,7 +18,7 @@
             <li <?php if(is_page('about-us') or wp_get_post_parent_id(get_the_ID(0)==6)) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
             <li><a href="<?php echo site_url('/programs'); ?>">Programs</a></li>
             <li><a href="<?php echo site_url('/events'); ?>">Events</a></li>
-            <li><a href="<?php echo site_url('/campuses'); ?>">Campuses</a></li>
+            <li><a href="<?php echo get_post_type_archive_link('campus'); ?>">Campuses</a></li>
             <li <?php if(get_post_type()=='post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
           </ul>
         </nav>
