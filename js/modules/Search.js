7bb387b728493a85b93105e5f416c897fc7ac6dc
Phone <phone.madridista@gmail.com>
2018-11-09 13:15:37 +0700

    Open and search overlay and fix some old code

---
+++ js/modules/Search.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+class Search{
+    // 1.descrip and create/initiate our object
+    constructor(){
+        this.openButton = $(".js-search-trigger");
+        this.closeButton = $('.search-overlay__close');
+        this.searchOverlay = $(".search-overlay");
+        this.events();
+    }
+    // 2. events
+    events(){
+        this.openButton.on("click", this.openOverlay.bind(this));
+        this.closeButton.on("click", this.closeOverlay.bind(this));
+    }
+
+    // 3. methods(functions/actions) 
+    openOverlay(){
+        this.searchOverlay.addClass("search-overlay--active");
+    }
+    closeOverlay(){
+        this.searchOverlay.removeClass("search-overlay--active");
+    }
+}
+export default Search; // Allow us to import this code to main script file
