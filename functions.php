27175d28c2b6505e299e140daa05bb84ae6bcc94
Phone <phone.madridista@gmail.com>
2018-11-10 08:14:24 +0700

    Keyboard event in javascript

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function university_files(){
     //wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js', NULL, microtime(), true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     wp_enqueue_style('university_main_style', get_stylesheet_uri(),NULL,microtime()); //Enqueue style.css
     wp_enqueue_style('font-awesome', get_theme_file_uri('/font-awesome/css/font-awesome.min.css'));
 }
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -6,19 +6,33 @@ class Search{
         this.closeButton = $('.search-overlay__close');
         this.searchOverlay = $(".search-overlay");
         this.events();
+        this.isOverlayOpen = false;
     }
     // 2. events
     events(){
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
+        $(document).on("keydown", this.keyPressDispatcher.bind(this));
     }
 
     // 3. methods(functions/actions) 
     openOverlay(){
         this.searchOverlay.addClass("search-overlay--active");
+        $("body").addClass("body-no-scroll");
+        this.isOverlayOpen = true;
     }
     closeOverlay(){
         this.searchOverlay.removeClass("search-overlay--active");
+        $("body").removeClass("body-no-scroll");
+        this.isOverlayOpen = false;
+    }
+    keyPressDispatcher(e){
+        if(e.keyCode == 83 && !this.isOverlayOpen){
+            this.openOverlay();
+        }
+        if(e.keyCode == 27 && this.isOverlayOpen){
+            this.closeOverlay();
+        }
     }
 }
 export default Search; // Allow us to import this code to main script file
