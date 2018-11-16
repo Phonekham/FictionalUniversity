601c611edfe18252852bdd71228fa0fdc4a2ad68
Phonekham <30440409+Phonekham@users.noreply.github.com>
2018-11-16 20:41:12 +0700

    090 Enforce Limit of One Like Per User_Teacher Combo

--- single-professor.php
+++ single-professor.php
@@ -25,20 +25,23 @@ while(have_posts()){
                     )); 
 
                     $existsStatus = 'no';
-                    $existsQuery = new WP_Query(array(
-                        'author'     =>  get_current_user_id(),
-                        'post_type'     =>  'like',
-                        'meta_query'      =>  array(
-                            array(
-                                'key'       =>  'liked_professor_id',
-                            'compare'       =>  '=',
-                            'value'         =>  get_the_ID()
+                    if(is_user_logged_in()){
+                        $existsQuery = new WP_Query(array(
+                            'author'     =>  get_current_user_id(),
+                            'post_type'     =>  'like',
+                            'meta_query'      =>  array(
+                                array(
+                                    'key'       =>  'liked_professor_id',
+                                'compare'       =>  '=',
+                                'value'         =>  get_the_ID()
+                                )
                             )
-                        )
-                    )); 
-                    if ($existsQuery->found_posts) {
-                        $existsStatus = 'yes';
+                        )); 
+                        if ($existsQuery->found_posts) {
+                            $existsStatus = 'yes';
+                        }
                     }
+                  
                     ?>
                     <span class="like-box" data-professor="<?php the_ID(); ?>" data-exists="<?php echo $existsStatus; ?>">
                         <i class="fa fa-heart-o" aria-hidden="true"></i>
