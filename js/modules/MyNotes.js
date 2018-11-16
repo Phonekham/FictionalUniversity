6a8be31eae8257d0daac784a62a9d85109c60f95
Phonekham <30440409+Phonekham@users.noreply.github.com>
2018-11-16 10:45:30 +0700

    Delet note with the rest api & edit button

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -5,17 +5,20 @@ class MyNotes{
     }
     events(){
         $(".delete-note").on("click", this.deleteNote)
+        $(".edit-note").on("click", this.editNote)
     }
 
     //Method
-    deleteNote(){
+    deleteNote(e){
+        var thisNote = $(e.target).parents('li');
         $.ajax({
             beforeSend: (xhr)   =>  {
                 xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
             },
-            url: universityData.root_url + '/wp-json/wp/v2/note/97',
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
             type: 'DELETE',
             success: (Response) => {
+                thisNote.slideUp()
                 console.log("congrates");
                 console.log(Response);
             },
@@ -24,6 +27,11 @@ class MyNotes{
                 console.log(Response);
             } 
         });
+    }
+    editNote(e){
+        var thisNote = $(e.target).parents('li');
+        thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+        thisNote.find('.update-note').addClass("update-note--visible");
     }
 }
 export default MyNotes;
--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13763,18 +13763,22 @@ function () {
     key: "events",
     value: function events() {
       (0, _jquery.default)(".delete-note").on("click", this.deleteNote);
+      (0, _jquery.default)(".edit-note").on("click", this.editNote);
     } //Method
 
   }, {
     key: "deleteNote",
-    value: function deleteNote() {
+    value: function deleteNote(e) {
+      var thisNote = (0, _jquery.default)(e.target).parents('li');
+
       _jquery.default.ajax({
         beforeSend: function beforeSend(xhr) {
           xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
         },
-        url: universityData.root_url + '/wp-json/wp/v2/note/97',
+        url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
         type: 'DELETE',
         success: function success(Response) {
+          thisNote.slideUp();
           console.log("congrates");
           console.log(Response);
         },
@@ -13783,6 +13787,13 @@ function () {
           console.log(Response);
         }
       });
+    }
+  }, {
+    key: "editNote",
+    value: function editNote(e) {
+      var thisNote = (0, _jquery.default)(e.target).parents('li');
+      thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+      thisNote.find('.update-note').addClass("update-note--visible");
     }
   }]);
 
